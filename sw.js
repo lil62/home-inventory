0340a3b1cea76cb6551f25d823d14757f2d0964b
lil62 <mutsuhirotakishima@gmail.com>
2026-06-28 12:22:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Service Worker - おうち在庫管理 PWA
+const CACHE_NAME = 'hinv-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
